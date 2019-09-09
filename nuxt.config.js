4b1c08ecd7b907d701a56f85ac385ad1f8ce3200
juniarsofyan <choerulsofyanmf@gmail.com>
2019-09-09 13:58:14 +0700

    Initial commit & setup

---
+++ nuxt.config.js
@@ -0,0 +1,79 @@
+/* eslint-disable prettier/prettier */
+require('dotenv').config()
+
+export default {
+    mode: 'spa',
+    /*
+     ** Headers of the page
+     */
+    head: {
+        title: process.env.npm_package_name || '',
+        meta: [{
+                charset: 'utf-8'
+            },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1'
+            },
+            {
+                hid: 'description',
+                name: 'description',
+                content: process.env.npm_package_description || ''
+            }
+        ],
+        link: [{
+            rel: 'icon',
+            type: 'image/x-icon',
+            href: '/favicon.ico'
+        }]
+    },
+    /*
+     ** Customize the progress-bar color
+     */
+    loading: {
+        color: '#fff'
+    },
+    /*
+     ** Global CSS
+     */
+    css: [],
+    /*
+     ** Plugins to load before mounting the App
+     */
+    plugins: [],
+    /*
+     ** Nuxt.js dev-modules
+     */
+    devModules: [
+        // Doc: https://github.com/nuxt-community/eslint-module
+        // '@nuxtjs/eslint-module'
+    ],
+    /*
+     ** Nuxt.js modules
+     */
+    modules: [
+        // Doc: https://axios.nuxtjs.org/usage
+        '@nuxtjs/axios',
+        '@nuxtjs/pwa',
+        // https://github.com/nuxt-community/dotenv-module
+        ['@nuxtjs/dotenv', {
+            filename: '.env'
+        }]
+    ],
+    /*
+     ** Axios module configuration
+     ** See https://axios.nuxtjs.org/options
+     */
+    axios: {
+        baseURL: process.env.API_BASE_URL
+    },
+    /*
+     ** Build configuration
+     */
+    build: {
+        /*
+         ** You can extend webpack config here
+         */
+        extend(config, ctx) {}
+    }
+}
