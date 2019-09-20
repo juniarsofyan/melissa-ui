5cd7463b047f3ef9bfa25141f0baf4d18fab3cb5
Choerul Sofyan Muhammad Falih <choerulsofyanmf@gmail.com>
2019-09-20 16:25:36 +0700

    Minor update - Add promo menu in header component

--- components/Header.vue
+++ components/Header.vue
@@ -234,6 +234,9 @@
                             <nuxt-link to="/products/extra care" tag="li" class="menu-item">
                                 <a href="#" class="kt-item-title" title="Extra Care">Extra Care</a>
                             </nuxt-link>
+                            <nuxt-link to="/products/promoted" tag="li" class="menu-item">
+                                <a href="#" class="kt-item-title" title="Promoted">Promoted</a>
+                            </nuxt-link>
                         </ul>
                     </div>
                 </div>
