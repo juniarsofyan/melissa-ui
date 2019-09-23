0844c78c4d00391fe5c8c5b23a7cb4a3730d446e
Choerul Sofyan Muhammad Falih <choerulsofyanmf@gmail.com>
2019-09-23 09:51:57 +0700

    Minor updates

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,147 +1,156 @@
 <template>
-  <div>
-    <footer class="footer default">
-      <div class="container">
-        <div class="container-wapper">
-          <div class="row">
-            <div class="box-footer col-xs-12 col-sm-6 col-md-6 col-lg-4">
-              <div class="widget-box">
-                <div class="single-img">
-                  <a href="index.html">
-                    <img src="~/assets/img/logo-bellezkin.png" width="200px" alt="Bellezkin" />
-                  </a>
-                </div>
-                <p>&nbsp;</p>
-                <div class="text-content-elememnt">
-                  <p>
-                    Shop the latest products right from
-                    your home with TuranOutfit.
-                  </p>
-                  <p>We have beard supplies & accessories from top brands.</p>
-                </div>
-              </div>
-            </div>
-            <div class="box-footer col-xs-6 col-sm-6 col-md-6 col-lg-2">
-              <div class="turan-custommenu default">
-                <h2 class="widgettitle">Links</h2>
-                <ul class="menu">
-                  <li class="menu-item">
-                    <a href="#">Home</a>
-                  </li>
-                  <li class="menu-item">
-                    <a href="#">Brightening</a>
-                  </li>
-                  <li class="menu-item">
-                    <a href="#">Purify</a>
-                  </li>
-                  <li class="menu-item">
-                    <a href="#">Decorative</a>
-                  </li>
-                  <li class="menu-item">
-                    <a href="#">Extra Care</a>
-                  </li>
-                  <li class="menu-item">
-                    <a href="#">Promo</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div class="box-footer col-xs-6 col-sm-6 col-md-6 col-lg-2">
-              <div class="turan-custommenu default">
-                <h2 class="widgettitle">Information</h2>
-                <ul class="menu">
-                  <li class="menu-item">
-                    <a href="#">FAQs</a>
-                  </li>
-                  <li class="menu-item">
-                    <a href="#">Track Order</a>
-                  </li>
-                  <li class="menu-item">
-                    <a href="#">Delivery</a>
-                  </li>
-                  <li class="menu-item">
-                    <a href="#">Contact Us</a>
-                  </li>
-                  <li class="menu-item">
-                    <a href="#">Return</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div class="box-footer col-xs-12 col-sm-6 col-md-6 col-lg-4">
-              <div class="turan-newsletter style1">
-                <div class="newsletter-head">
-                  <h3 class="title">Subscribe our Newsletter</h3>
-                </div>
-                <div class="newsletter-form-wrap">
-                  <div class="list">
-                    <span class="text">Im interested in:</span>
-                    <label for="radio-footer-1">
-                      <input
-                        checked
-                        id="radio-footer-1"
-                        type="radio"
-                        name="list_id"
-                        value="radio-footer-1"
-                      />
+    <div>
+        <footer class="footer default">
+            <div class="container">
+                <div class="container-wapper">
+                    <div class="row">
+                        <div class="box-footer col-xs-12 col-sm-6 col-md-6 col-lg-4">
+                            <div class="widget-box">
+                                <div class="single-img">
+                                    <a href="index.html">
+                                        <img
+                                            src="~/assets/img/logo-bellezkin.png"
+                                            width="200px"
+                                            alt="Bellezkin"
+                                        />
+                                    </a>
+                                </div>
+                                <p>&nbsp;</p>
+                                <div class="text-content-elememnt">
+                                    <p>
+                                        Shop the latest products right from
+                                        your home with TuranOutfit.
+                                    </p>
+                                    <p>We have beard supplies & accessories from top brands.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="box-footer col-xs-6 col-sm-6 col-md-6 col-lg-2">
+                            <div class="turan-custommenu default">
+                                <h2 class="widgettitle">Links</h2>
+                                <ul class="menu">
+                                    <li class="menu-item">
+                                        <a href="#">Home</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="#">Brightening</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="#">Purify</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="#">Decorative</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="#">Extra Care</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="#">Promo</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="box-footer col-xs-6 col-sm-6 col-md-6 col-lg-2">
+                            <div class="turan-custommenu default">
+                                <h2 class="widgettitle">Information</h2>
+                                <ul class="menu">
+                                    <li class="menu-item">
+                                        <a href="#">FAQs</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="#">Track Order</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="#">Delivery</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="#">Contact Us</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="#">Return</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="box-footer col-xs-12 col-sm-6 col-md-6 col-lg-4">
+                            <div class="turan-newsletter style1">
+                                <div class="newsletter-head">
+                                    <h3 class="title">Subscribe our Newsletter</h3>
+                                </div>
+                                <div class="newsletter-form-wrap">
+                                    <div class="list">
+                                        <span class="text">Im interested in:</span>
+                                        <label for="radio-footer-1">
+                                            <input
+                                                checked
+                                                id="radio-footer-1"
+                                                type="radio"
+                                                name="list_id"
+                                                value="radio-footer-1"
+                                            />
 
-                      <span class="text">Accessories</span>
-                    </label>
-                    <label for="radio-footer-2">
-                      <input id="radio-footer-2" type="radio" name="list_id" value="radio-footer-2" />
+                                            <span class="text">Accessories</span>
+                                        </label>
+                                        <label for="radio-footer-2">
+                                            <input
+                                                id="radio-footer-2"
+                                                type="radio"
+                                                name="list_id"
+                                                value="radio-footer-2"
+                                            />
 
-                      <span class="text">Grooming</span>
-                    </label>
-                  </div>
-                  <input
-                    type="email"
-                    class="input-text email email-newsletter"
-                    placeholder="Your email letter"
-                  />
-                  <button class="button btn-submit submit-newsletter">SUBSCRIBE</button>
+                                            <span class="text">Grooming</span>
+                                        </label>
+                                    </div>
+                                    <input
+                                        type="email"
+                                        class="input-text email email-newsletter"
+                                        placeholder="Your email letter"
+                                    />
+                                    <button class="button btn-submit submit-newsletter">SUBSCRIBE</button>
+                                </div>
+                            </div>
+                            <div class="turan-socials">
+                                <ul class="socials">
+                                    <li>
+                                        <a href="#" class="social-item" target="_blank">
+                                            <i class="icon fa fa-facebook-square"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="social-item" target="_blank">
+                                            <i class="icon fa fa-twitter"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="social-item" target="_blank">
+                                            <i class="icon fa fa-instagram"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-12 border-custom">
+                            <span></span>
+                        </div>
+                    </div>
+                    <div class="footer-end">
+                        <div class="row">
+                            <div>
+                                <center>
+                                    <div class="coppyright">
+                                        Copyright © 2019
+                                        <a href="https://bellezkin.com">Bellezkin</a>
+                                        . All rights reserved
+                                    </div>
+                                </center>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
-              <div class="turan-socials">
-                <ul class="socials">
-                  <li>
-                    <a href="#" class="social-item" target="_blank">
-                      <i class="icon fa fa-facebook-square"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" class="social-item" target="_blank">
-                      <i class="icon fa fa-twitter"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" class="social-item" target="_blank">
-                      <i class="icon fa fa-instagram"></i>
-                    </a>
-                  </li>
-                </ul>
-              </div>
             </div>
-          </div>
-          <div class="row">
-            <div class="col-sm-12 border-custom">
-              <span></span>
-            </div>
-          </div>
-          <div class="footer-end">
-            <div class="row">
-              <div>
-                <center>
-                  <div class="coppyright">
-                    Copyright © 2019
-                    <a href="https://bellezkin.com">Bellezkin</a>
-                    . All rights reserved
-                  </div>
-                </center>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </footer>
-  </div>
+        </footer>
+    </div>
 </template>
