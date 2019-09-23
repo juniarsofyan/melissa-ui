620652dae7576e4da185de8388262d0143beadf8
Recito Gigih P <recitogigih9@gmail.com>
2019-09-23 23:52:19 +0700

    -

--- pages/products/_code/detail.vue
+++ pages/products/_code/detail.vue
@@ -1,22 +1,18 @@
 <template>
 <div>
-	     <!-- breadcrumb -->
-                <div class="breadcrumb-trail breadcrumbs">
-                    <ul class="trail-items breadcrumb">
-                        <li class="trail-item trail-begin">
-                            <a href="">
-                                <span>
-                                    Home
-                                </span>
-                            </a>
-                        </li>
-                        <li class="trail-item trail-end active">
-                            <span>
-                                Shopping Cart
-                            </span>
-                        </li>
-                    </ul>
-                </div>
+	<!-- breadcrumb -->
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="breadcrumb-trail breadcrumbs">
+						<ul class="trail-items breadcrumb">
+							<li class="trail-item trail-begin"><a href="index.html">Home</a></li>
+							<li class="trail-item"><a href="#">Clothing</a></li>
+							<li class="trail-item trail-end active">3-pack T-shirts Regular fit</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+
   <!-- main content -->
 			<div class="row">
 
