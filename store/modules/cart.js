d2c9fcaa553f1f4021d59fb587a77406879ae4e7
Choerul Sofyan Muhammad Falih <choerulsofyanmf@gmail.com>
2019-09-23 16:10:38 +0700

    Cart functionalities - On menu bar cart - Count subtotal

--- store/modules/cart.js
+++ store/modules/cart.js
@@ -11,28 +11,40 @@ const cart = {
 
             if (item_exists) {
                 item_exists.qty = parseInt(item_exists.qty) + parseInt(item.qty)
-                item_exists.subtotal = item_exists.qty * item_exists.price
-                item_exists.grand_total = item_exists.grand_total + item.price_discount * item.qty
+                item_exists.subtotal = parseInt(item_exists.qty) * parseInt(item_exists.price)
+                item_exists.grand_total = parseInt(item_exists.grand_total) + parseInt(item.price_discount) * parseInt(item.qty)
                 item_exists.total_weight = item_exists.qty * item_exists.weight
             } else {
                 let product = {
                     product_code: item.kode_barang,
                     product_name: item.nama,
                     weight: item.berat,
-                    price: item.harga,
+                    price: parseInt(item.harga),
                     price_bonus_calculation: item.h_hpb,
                     discount: item.diskon,
-                    price_discount: item.harga_diskon,
-                    qty: item.qty,
-                    subtotal: item.price * item.qty,
-                    grand_total: item.harga_diskon * item.qty,
-                    total_weight: item.qty * item.berat,
+                    price_discount: parseInt(item.harga_diskon),
+                    qty: parseInt(item.qty),
+                    subtotal: parseInt(item.harga) * parseInt(item.qty),
+                    grand_total: parseInt(item.harga_diskon) * parseInt(item.qty),
+                    total_weight: parseInt(item.qty) * item.berat,
                     image: item.pic,
                     unit: item.unit,
+                    category: item.jenis,
+                    picture: item.pic ? item.pic : "",
                     note: item.note ? item.note : ""
                 }
 
                 state.items.push(product)
+            }
+
+            window.localStorage.setItem('items', JSON.stringify(state.items))
+        },
+        removeItem(state, product_code) {
+            let item = state.items.find(product => product.product_code == product_code)
+
+            if (item) {
+                let index = state.items.indexOf(item)
+                state.items.splice(index, 1)
             }
 
             window.localStorage.setItem('items', JSON.stringify(state.items))
@@ -41,6 +53,12 @@ const cart = {
     getters: {
         items(state) {
             return state.items
+        },
+        count(state) {
+            return state.items.reduce((accumulator, item) => accumulator + parseInt(item.qty), 0)
+        },
+        subtotal(state) {
+            return state.items.reduce((accumulator, item) => accumulator + parseInt(item.subtotal), 0)
         }
     },
     actions: {
@@ -52,6 +70,15 @@ const cart = {
             payload
         ) {
             commit('addItem', payload)
+        },
+        removeItem(
+            {
+                state,
+                commit
+            },
+            payload
+        ) {
+            commit('removeItem', payload)
         }
     }
 }
