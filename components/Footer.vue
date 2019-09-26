7506ffa773bc86b67e0713b39fb9dac13831e8c0
Choerul Sofyan Muhammad Falih <choerulsofyanmf@gmail.com>
2019-09-26 13:19:02 +0700

    Fix Footer.vue

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,145 +1,148 @@
 <template>
-  <div>
-    <footer class="footer default">
-      <div class="container">
-        <div class="container-wapper">
-          <div class="row">
-            <div class="box-footer col-xs-12 col-sm-6 col-md-6 col-lg-4">
-              <div class="widget-box">
-                <div class="single-img">
-                  <a href="index.html">
-                    <img src="~/assets/img/logo-bellezkin.png" width="200px" alt="Bellezkin" />
-                  </a>
+    <div>
+        <footer class="footer default">
+            <div class="container">
+                <div class="container-wapper">
+                    <div class="row">
+                        <div class="box-footer col-xs-12 col-sm-6 col-md-6 col-lg-4">
+                            <div class="widget-box">
+                                <div class="single-img">
+                                    <a href="index.html">
+                                        <img src="~/assets/img/logo-bellezkin.png" width="200px" alt="Bellezkin" />
+                                    </a>
+                                </div>
+                                <br />
+                                <div class="text-content-elememnt">
+                                    <p
+                                        id="about-us"
+                                    >Bellezkin adalah brand perawatan kecantikan yang berdiri tahun 2014. Mengutamakan pelayanan konsultasi dengan beauty consultant professional dan dengan dukungan tenaga ahli.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="box-footer col-xs-6 col-sm-6 col-md-6 col-lg-2">
+                            <div class="turan-custommenu default">
+                                <h2 class="widgettitle">Links</h2>
+                                <ul class="menu">
+                                    <li class="menu-item">
+                                        <a href="/">Home</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="./brightening">Brightening</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="./purify">Purify</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="./decorative">Decorative</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="./extra care">Extra Care</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="./promo">Promo</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="box-footer col-xs-6 col-sm-6 col-md-6 col-lg-2">
+                            <div class="turan-custommenu default">
+                                <h2 class="widgettitle">Information</h2>
+                                <ul class="menu">
+                                    <li class="menu-item">
+                                        <a href="/faq">FAQ's</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a
+                                            href="https://api.whatsapp.com/send?phone=628112288142&text=Halo!%0ASaya%20ingin%20ingin%20konsultasi%20lebih%20lanjut%20mengenai%20produk%20Bellezkin"
+                                            target="_blank"
+                                        >Beauty Consultant</a>
+                                    </li>
+                                    <li class="menu-item">
+                                        <a href="/return-policy">Return Policy</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="box-footer col-xs-12 col-sm-6 col-md-6 col-lg-4">
+                            <div class="turan-newsletter style1">
+                                <div class="newsletter-head">
+                                    <h3 class="title">Follow Us</h3>
+                                </div>
+                            </div>
+                            <div class="turan-socials">
+                                <ul class="socials">
+                                    <li>
+                                        <a
+                                            href="https://www.facebook.com/bellezkincare.natural/"
+                                            class="social-item"
+                                            target="_blank"
+                                            >
+                                            <i class="icon fa fa-facebook-square"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#" class="social-item" target="_blank">
+                                            <i class="icon fa fa-twitter"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            href="https://www.instagram.com/bellezkin/"
+                                            class="social-item"
+                                            target="_blank"
+                                            >
+                                            <i class="icon fa fa-instagram"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            href="https://www.youtube.com/channel/UCE0Hte0fUMeWhGrVn5zgwRQ"
+                                            class="social-item"
+                                            target="_blank"
+                                            >
+                                            <i class="icon fa fa-youtube"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                            <p>&nbsp;</p>
+                            <a
+                                href="https://cekbpom.pom.go.id/index.php/home/produk/vs9849n2dm48o0fiffkavi2d92/all/row/10/page/1/order/4/DESC/search/2/bellezkin"
+                                target="_blank"
+                                >
+                                <img src="../assets/img/bpom.png" id="logo-comp" />
+                            </a>
+                            <a
+                                href="http://www.halalmui.org/mui14/index.php/main/ceklogin_halal/produk_halal_masuk/1"
+                                target="_blank"
+                                >
+                                <img src="../assets/img/logo-halal.png" id="logo-comp" />
+                            </a>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <!--
+                        <div class="col-sm-12 border-custom">
+                            <span></span>
+                        </div>
+                        -->
+                        <p>&nbsp;</p>
+                        <p>&nbsp;</p>
+                    </div>
+                    <div class="footer-end">
+                        <div class="row">
+                            <div>
+                                <center>
+                                <div class="coppyright">
+                                    Copyright © 2019
+                                    <a href="https://bellezkin.com">Bellezkin</a>
+                                    . All rights reserved
+                                </div>
+                                </center>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <br />
-                <div class="text-content-elememnt">
-                  <p
-                    id="about-us"
-                  >Bellezkin adalah brand perawatan kecantikan yang berdiri tahun 2014. Mengutamakan pelayanan konsultasi dengan beauty consultant professional dan dengan dukungan tenaga ahli.</p>
-                </div>
-              </div>
-            </div>
-            <div class="box-footer col-xs-6 col-sm-6 col-md-6 col-lg-2">
-              <div class="turan-custommenu default">
-                <h2 class="widgettitle">Links</h2>
-                <ul class="menu">
-                  <li class="menu-item">
-                    <a href="/">Home</a>
-                  </li>
-                  <li class="menu-item">
-                    <a href="./brightening">Brightening</a>
-                  </li>
-                  <li class="menu-item">
-                    <a href="./purify">Purify</a>
-                  </li>
-                  <li class="menu-item">
-                    <a href="./decorative">Decorative</a>
-                  </li>
-                  <li class="menu-item">
-                    <a href="./extra care">Extra Care</a>
-                  </li>
-                  <li class="menu-item">
-                    <a href="./promo">Promo</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div class="box-footer col-xs-6 col-sm-6 col-md-6 col-lg-2">
-              <div class="turan-custommenu default">
-                <h2 class="widgettitle">Information</h2>
-                <ul class="menu">
-                  <li class="menu-item">
-                    <a href="/faq">FAQ's</a>
-                  </li>
-                  <li class="menu-item">
-                    <a
-                      href="https://api.whatsapp.com/send?phone=628112288142&text=Halo!%0ASaya%20ingin%20ingin%20konsultasi%20lebih%20lanjut%20mengenai%20produk%20Bellezkin"
-                      target="_blank"
-                    >Beauty Consultant</a>
-                  </li>
-                  <li class="menu-item">
-                    <a href="/return-policy">Return Policy</a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div class="box-footer col-xs-12 col-sm-6 col-md-6 col-lg-4">
-              <div class="turan-newsletter style1">
-                <div class="newsletter-head">
-                  <h3 class="title">Follow Us</h3>
-                </div>
-              </div>
-              <div class="turan-socials">
-                <ul class="socials">
-                  <li>
-                    <a
-                      href="https://www.facebook.com/bellezkincare.natural/"
-                      class="social-item"
-                      target="_blank"
-                    >
-                      <i class="icon fa fa-facebook-square"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#" class="social-item" target="_blank">
-                      <i class="icon fa fa-twitter"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="https://www.instagram.com/bellezkin/"
-                      class="social-item"
-                      target="_blank"
-                    >
-                      <i class="icon fa fa-instagram"></i>
-                    </a>
-                  </li>
-                  <li>
-                    <a
-                      href="https://www.youtube.com/channel/UCE0Hte0fUMeWhGrVn5zgwRQ"
-                      class="social-item"
-                      target="_blank"
-                    >
-                      <i class="icon fa fa-youtube"></i>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-              <p>&nbsp;</p>
-              <a
-                href="https://cekbpom.pom.go.id/index.php/home/produk/vs9849n2dm48o0fiffkavi2d92/all/row/10/page/1/order/4/DESC/search/2/bellezkin"
-                target="_blank"
-              >
-                <img src="../assets/img/bpom.png" id="logo-comp" />
-              </a>
-              <a
-                href="http://www.halalmui.org/mui14/index.php/main/ceklogin_halal/produk_halal_masuk/1"
-                target="_blank"
-              >
-                <img src="../assets/img/logo-halal.png" id="logo-comp" />
-              </a>
-            </div>
-          </div>
-          <div class="row">
-            <!--
-            <div class="col-sm-12 border-custom">
-              <span></span>
-            </div>
-            -->
-            <p>&nbsp;</p>
-            <p>&nbsp;</p>
-          </div>
-          <div class="footer-end">
-            <div class="row">
-              <div>
-                <center>
-                  <div class="coppyright">
-                    Copyright © 2019
-                    <a href="https://bellezkin.com">Bellezkin</a>
-                    . All rights reserved
-                  </div>
-                </center>
-              </div>
             </div>
         </footer>
     </div>
