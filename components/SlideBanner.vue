e3c907c3fc8e7649e27e1c3cf01f50a2830b4e2b
Choerul Sofyan Muhammad Falih <choerulsofyanmf@gmail.com>
2019-10-01 13:53:29 +0700

    Change Slider text

--- components/SlideBanner.vue
+++ components/SlideBanner.vue
@@ -6,15 +6,11 @@
         <div class="item-banner style1">
             <div class="inner equal-element">
                 <div class="banner-content style1">
-                    <h4 class="turan-subtitle">Start ur weekend off right!</h4>
+                    <h4 class="turan-subtitle">Delicately Moisturizing Your Skin</h4>
                     <h3 class="title">
-                        T-shirt Sale
-                        <br />15% OFF
+                        Enhanced with
+                        <br />Vitamin C
                     </h3>
-                    <span class="when-code">
-                        When Use Code:
-                        <span class="code">Z9QAR25</span>
-                    </span>
                     <button class="button button-now">shop now</button>
                 </div>
             </div>
@@ -22,17 +18,9 @@
         <div class="item-banner style2">
             <div class="inner equal-element">
                 <div class="banner-content style2">
-                    <h3 class="title">Little America™ Backpack</h3>
-                    <span class="description">
-                        Suit your style with our new
-                        <br />Backpack collection
-                    </span>
-                    <span class="hot-price">
-                        Hot Price:
-                        <span class="price-number">€250.00</span>
-                    </span>
+                    <span class="description">Rid Of Pustule</span>
+                    <h3 class="title">Serum Acne Tea Tree Oil</h3>
                     <button class="button button-now">shop now</button>
-                    <button class="button button-view">view collection</button>
                 </div>
             </div>
         </div>
