f2266d2e1addc032cf25baf83f27337c7d805dff
Choerul Sofyan Muhammad Falih <choerulsofyanmf@gmail.com>
2020-01-11 10:59:30 +0700

    Fix SPB

--- pages/checkout.vue
+++ pages/checkout.vue
@@ -292,7 +292,8 @@
                 delivery_address: false,
                 default_shipping_address: "",
                 shipping_addresses: [],
-                sales_branches: [{
+                sales_branches: [
+                    {
                         code: '00000',
                         province_id: 9,
                         city_id: 23,
@@ -300,6 +301,26 @@
                         subdistrict_id: 359,
                         subdistrict_name: 'Lengkong',
                         phone: '+6282119163629',
+                        disabled: true
+                    },
+                    {
+                        code: '14',
+                        province_id: 9,
+                        city_id: 23,
+                        city_name: 'BANDUNG',
+                        subdistrict_id: 361,
+                        subdistrict_name: 'Panyileukan',
+                        phone: '-',
+                        disabled: true
+                    },
+                    {
+                        code: '13722',
+                        province_id: 9,
+                        city_id: 54,
+                        city_name: 'BEKASI',
+                        subdistrict_id: 733,
+                        subdistrict_name: 'Cikarang Selatan',
+                        phone: '-',
                         disabled: true
                     },
                     {
@@ -323,6 +344,26 @@
                         disabled: true
                     },
                     {
+                        code: '05624',
+                        province_id: 9,
+                        city_id: 104,
+                        city_name: 'CIANJUR',
+                        subdistrict_id: 1415,
+                        subdistrict_name: 'Cikalong',
+                        phone: '-',
+                        disabled: true
+                    },
+                    {
+                        code: '15658',
+                        province_id: 9,
+                        city_id: 115,
+                        city_name: 'DEPOK',
+                        subdistrict_id: 1585,
+                        subdistrict_name: 'Sawangan',
+                        phone: '-',
+                        disabled: true
+                    },
+                    {
                         code: '00539',
                         province_id: 34,
                         city_id: 278,
@@ -340,6 +381,27 @@
                         subdistrict_id: 5954,
                         subdistrict_name: 'Pelabuhan/Palabuhan Ratu',
                         phone: '+6281337479174',
+                        disabled: true
+                    },
+                    {
+                        code: '01835',
+                        province_id: 9,
+                        city_id: 376,
+                        city_name: 'PURWAKARTA',
+                        subdistrict_old_id: 5954,
+                        subdistrict_id: 5218,
+                        subdistrict_name: 'Bojong',
+                        phone: '-',
+                        disabled: true
+                    },
+                    {
+                        code: '01838',
+                        province_id: 10,
+                        city_id: 41,
+                        city_name: 'PURWOKERTO',
+                        subdistrict_id: 591,
+                        subdistrict_name: 'Purwokerto Timur',
+                        phone: '+6283818235538',
                         disabled: true
                     },
                     {
@@ -363,62 +425,12 @@
                         disabled: true
                     },
                     {
-                        code: '01838',
-                        province_id: 10,
-                        city_id: 41,
-                        city_name: 'PURWOKERTO',
-                        subdistrict_id: 591,
-                        subdistrict_name: 'Purwokerto Timur',
-                        phone: '+6283818235538',
-                        disabled: true
-                    },
-                    {
-                        code: '15666',
+                        code: '00004',
                         province_id: 3,
                         city_id: 457,
                         city_name: 'TANGERANG SELATAN',
-                        subdistrict_id: 6312,
-                        subdistrict_name: 'Pamulang',
-                        phone: '-',
-                        disabled: true
-                    },
-                    {
-                        code: '15658',
-                        province_id: 9,
-                        city_id: 115,
-                        city_name: 'DEPOK',
-                        subdistrict_id: 1585,
-                        subdistrict_name: 'Sawangan',
-                        phone: '-',
-                        disabled: true
-                    },
-                    {
-                        code: '15641',
-                        province_id: 6,
-                        city_id: 154,
-                        city_name: 'Jakarta Timur',
-                        subdistrict_id: 2116,
-                        subdistrict_name: 'Duren Sawit',
-                        phone: '-',
-                        disabled: true
-                    },
-                    {
-                        code: '13722',
-                        province_id: 9,
-                        city_id: 54,
-                        city_name: 'Bekasi',
-                        subdistrict_id: 733,
-                        subdistrict_name: 'Cikarang Selatan',
-                        phone: '-',
-                        disabled: true
-                    },
-                    {
-                        code: '02006',
-                        province_id: 9,
-                        city_id: 126,
-                        city_name: 'Garut',
-                        subdistrict_id: 1751,
-                        subdistrict_name: 'Tarogong Kidul',
+                        subdistrict_id: 6313,
+                        subdistrict_name: 'Pondok Aren',
                         phone: '-',
                         disabled: true
                     }
